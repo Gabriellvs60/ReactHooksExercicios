188756072a078cc6a0f1b1fc0981deb218192aff
Gabriellvs60 <gabriellvs60@gmail.com>
2019-12-20 11:25:53 -0300

    UseState with previous State

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import HookCounter from "./components/HookCounter";
+import HookCounterTwo from "./components/HookCounterTwo";
 
 const App: React.FC = () => {
  
   return (
     <>
       <div className="App">
-      <HookCounter/>
+      <HookCounterTwo/>
       </div>
     </>
   );
